e4ac1ea1876cba4433141b360de2e312ab500720
Sagar ghare <sagar.ghare@acc.ltd>
2023-09-05 12:05:17 +0530

    testing route added

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const server = express();
 const PORT = 8080;
 
+server.listen('/', (req, res) => {
+    res.status(200).json({
+        message: "SUCCESS"
+    });
+});
+
 server.listen(PORT, (err) => {
     if (err) throw err;
     console.log(`server listening on ${PORT}`);
